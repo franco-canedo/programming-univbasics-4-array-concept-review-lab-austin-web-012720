aa45e9daaed42dc1a60fd0633ddeb2174b61bbee
Franco Canedo <francanedo1@hotmail.com>
2020-01-07 00:37:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,8 +13,6 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  array.min_by do |index|
-    array[index]
-  end 
-  return array[index]
+  new_array = array.sort
+  return new_array.first
 end
